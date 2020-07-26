b44dd40795e2716015d146c0e44dc0cfb7e1ad50
Dhruv Mishra <dhruv.mishra@sezzle.com>
2020-07-26 14:17:02 +0530

    ennabled django admin for user

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -9,9 +9,9 @@ class UserProfileManager(BaseUserManager):
     def create_user(self, email, first_name, last_name, password=None):
         """ Create a new user profile """
         if not email:
-            raise  ValueError("User must  have an email")
+            raise  ValueError("User must  have an email");
 
-        email  = self.normailize_email(email)
+        email = self.normalize_email(email)
         user = self.model(email=email, first_name=first_name, last_name=last_name)
 
         user.set_password(password)
@@ -19,9 +19,9 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_superuser(email, first_name, last_name, password):
+    def create_superuser(self, email, first_name, last_name, password):
         """ Create super user with create_user function """
-        user =  self.create_user(email,first_name,last_name,password)
+        user =  self.create_user(email, first_name, last_name, password)
 
         user.is_superuser = True
         user.is_staff = True
@@ -29,26 +29,27 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
+
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """ Database model for user in the system """
-    email =  models.EmailField(max_length = 255, unique = True)
-    first_name =  models.CharField(max_length = 255)
-    last_name =  models.CharField(max_length = 255)
-    is_active =  models.BooleanField(default = True)
-    is_staff =  models.BooleanField(default = False)
+    email = models.EmailField(max_length = 255, unique = True)
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
 
     objects = UserProfileManager()
 
-    USERNAME_FIELD =  "email"
-    REQUIRED_FIELD =  ["first_name", "last_name"]
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     def get_full_name(self):
         """Retrive full name of the user """
-        return self.name + self.last_name
+        return self.first_name + self.last_name
 
     def get_short_name(self):
         """Retrive short name of the user """
-        return self.name
+        return self.first_name
 
     def _str_(self):
         """Return string representation  of a user """
